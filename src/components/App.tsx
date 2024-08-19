441087e43f4ffc314e6a4551ee07bb1cba830090
hexadeciman <yassinebenchekroune@gmail.com>
2024-08-19 18:53:39 +0100

    Simplify boilerplate, and add util functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,3 @@
-import Avatar from 'components/Avatar'
-import logo from 'assets/logo.svg'
-
 const randoms = [
 	[1, 2],
 	[3, 4, 5],
@@ -13,11 +10,8 @@ function App() {
 			<div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
 				<div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
 					<div className="sm:max-w-lg">
-						<div className="my-4">
-							<Avatar size="large" src={logo} />
-						</div>
 						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-							Welcome!
+							React Boilerplate
 						</h1>
 						<p className="mt-4 text-xl text-gray-500">
 							This is a boilerplate build with Vite, React 18,
@@ -31,7 +25,7 @@ function App() {
 								href="vscode://"
 								className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
 							>
-								Start building for free
+								Start building
 							</a>
 							<div
 								aria-hidden="true"
